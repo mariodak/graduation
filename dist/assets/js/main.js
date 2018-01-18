010057e83e0121266c05a68f97bdd310e15bb924
mariodak <e.mariodak@gmail.com>
2018-01-18 22:48:59 +0100

    update

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Main JS File
 
 // Blokuje Zoom na prehliadačoch
@@ -11,24 +13,57 @@ document.addEventListener('touchmove', function (event) {
 // Aktivuje effekt po scrollnutí --px
 
 // Added dynamic class by @fazulkovy
-const objects = $('.animation')
-function scrollAnimation(){
+var objects = $('.object');
+function scrollAnimation() {
 
-    const scroll         = $(window).scrollTop()
-    
-    
-    objects.each(function(index, object) {
-      const calcHeight     = $(window).height() - $(object).height()
-      const totalDistance  = $(object).offset().top  - calcHeight
-      const totalDistancex = calcHeight - $(object).offset().top
-      
-    if(scroll >= totalDistance)
-        $(object).addClass('apply')
-    else 
-        $(object).removeClass('apply')
-      
-      })
+    var scroll = $(window).scrollTop();
 
+    objects.each(function (index, object) {
+        var calcHeight = $(window).height() - $(object).height();
+        var totalDistance = $(object).offset().top - calcHeight;
+
+        if (scroll >= totalDistance) $(object).addClass('apply');else $(object).removeClass('apply');
+    });
 }
+scrollAnimation();
+$(window).scroll(scrollAnimation);
 
-// scroll
+// Menu loading
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        });
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top + 2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event) {
+    var scrollPos = $(document).scrollTop();
+    $('.menu .progressbar li a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.menu .progressbar li').removeClass(".menu .progressbar li .active");
+            currLink.addClass("active");
+        } else {
+            currLink.removeClass("active");
+        }
+    });
+}
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -11,24 +11,66 @@ document.addEventListener('touchmove', function (event) {
 // Aktivuje effekt po scrollnutí --px
 
 // Added dynamic class by @fazulkovy
-const objects = $('.animation')
-function scrollAnimation(){
+const objects = $('.object')
+function scrollAnimation() {
 
-    const scroll         = $(window).scrollTop()
-    
-    
-    objects.each(function(index, object) {
-      const calcHeight     = $(window).height() - $(object).height()
-      const totalDistance  = $(object).offset().top  - calcHeight
-      const totalDistancex = calcHeight - $(object).offset().top
-      
-    if(scroll >= totalDistance)
-        $(object).addClass('apply')
-    else 
-        $(object).removeClass('apply')
-      
-      })
+    const scroll = $(window).scrollTop()
+
+
+    objects.each(function (index, object) {
+        const calcHeight = $(window).height() - $(object).height()
+        const totalDistance = $(object).offset().top - calcHeight
+
+        if (scroll >= totalDistance)
+            $(object).addClass('apply')
+        else
+            $(object).removeClass('apply')
+
+    })
 
 }
+scrollAnimation()
+$(window).scroll(scrollAnimation);
 
-// scroll
+
+
+// Menu loading
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top + 2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event) {
+    var scrollPos = $(document).scrollTop();
+    $('.menu .progressbar li a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.menu .progressbar li').removeClass(".menu .progressbar li .active");
+            currLink.addClass("active");
+        }
+        else {
+            currLink.removeClass("active");
+        }
+    });
+}
